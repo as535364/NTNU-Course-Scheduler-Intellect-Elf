c22a2c82e22493b687f864941b8ed460b376a87e
as535364 <a6194315535@gmail.com>
2021-06-21 03:44:10 +0800

    register without email or username duplicate

--- routes/user.py
+++ routes/user.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from db import db
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from share.db import db
 from model import User
 
 user_bp = Blueprint('user_bp', __name__)
@@ -10,7 +10,17 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        print(request.form)
+        data = request.form
+        username = data.get('username')
+        password = data.get('password')
+        error = False
+        if not username:
+            flash('請輸入帳號', 'error')
+            error = True
+        if not password:
+            flash('請輸入密碼', 'error')
+            error = True
+
         return redirect(url_for('account.login'))
 
 
@@ -20,9 +30,30 @@ def register():
         return render_template('register.html')
     else:
         data = request.form
-        user = User(email=data.get('email'), username=data.get('username'),
-                    password=data.get('password'), dept_name=data.get('dept_name'), grade=data.get('grade'))
-        db.session.add(user)
-        db.session.commit()
-        print(user)
-        return redirect((url_for('user_bp.login')))
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        repeat_password = data.get('repeatPassword')
+        dept_name = data.get('dept_name') if data.get('dept_name') else ''
+        grade = data.get('grade') if data.get('grade') else 0
+        error = False
+        if not username:
+            flash('請輸入帳號', 'error')
+            error = True
+        if not email:
+            flash('請輸入 Email', 'error')
+            error = True
+        if not password or not repeat_password:
+            flash('請輸入密碼以及重複密碼', 'error')
+            error = True
+        if password != repeat_password:
+            flash('密碼以及重複密碼不一致', 'error')
+            error = True
+        if not error:
+            user = User(username=username, email=email, password=password, dept_name=dept_name, grade=grade)
+            print(user)
+            db.session.add(user)
+            db.session.commit()
+            return redirect((url_for('user_bp.login')))
+        else:
+            return render_template('register.html')
