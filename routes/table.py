2ad73c88aed57e2c849314da1b1e583f9398e90e
grace21738 <a30230226@gmail.com>
2021-06-22 17:36:12 +0800

    dropdown no value

--- routes/table.py
+++ routes/table.py
@@ -1,8 +1,33 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from share.db import db
+from model import Course
 
 table_bp = Blueprint('table_bp', __name__, static_folder='static')
 
 
-@table_bp.route('/show')
-def show():
-    return render_template('table.html')
+@table_bp.route('/table', methods=['GET', 'POST'])
+def table():
+    # get all courses data
+    course = Course.query.all()
+    # for item in course:
+    #     print(item.course_name)
+    return render_template('table.html', values=course)
+# return redirect(url_for("login"))
+
+
+@table_bp.route('/table/search', methods=['GET', 'POST'])
+def search_course():
+    tex = []
+    if request.method == 'POST':
+        print(request.form)
+        if request.form.get('condition') is not None:
+            tex.append(request.values['condition'])
+
+        tex.append(request.values.get('btnTypeTex'))
+        # if request.form.get('btnDeptTex') != '開課系所':
+        #     tex.append(request.values['btnDeptTex'])
+        print(tex)
+        # if len(tex) == 3:
+        return render_template('table.html')
+    else:
+        return render_template('table.html')
