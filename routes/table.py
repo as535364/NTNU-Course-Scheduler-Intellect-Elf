9342095faab3efb2902219e0844f13c7607c470c
grace21738 <a30230226@gmail.com>
2021-06-23 18:29:09 +0800

    course table done

--- routes/table.py
+++ routes/table.py
@@ -1,13 +1,63 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from share.login import login_required
+from collections import defaultdict
 from share.db import db
 from model import Course
 
 table_bp = Blueprint('table_bp', __name__, static_folder='static')
 
 
-def print_user(user):
-    print(user.username)
+def get_credits(user):
+
+    credit = [0, 0, 0]
+    for course in user.courses:
+        credit[0] += course.credits
+        if user.dept_name is not None and course.department == user.dept_name:
+            credit[1] += course.credits
+        elif course.dept_code == 'GU':
+            credit[2] += course.credits
+    return credit
+
+def spilt_time(s):
+    t = str(s).split(' ')
+    dic = {'一': 0, '二': 1, '三': 2, '四': 3, '五': 4, '六': 5 }
+    day = dic[t[0]]
+    time = t[1].split('-')
+    return day, time[0], time[1]
+
+
+def parse_course_time(courses):
+    table = []
+    for j in range(6):
+        table.append([])
+    for j in range(6):
+        for i in range(15):
+            table[j].append([])
+            # table[2][i + 1] = list()
+            # table[3][i + 1] = list()
+            # table[4][i + 1] = list()
+            # table[5][i + 1] = list()
+            # table['六'][i + 1] = list()
+    # print(type(day))
+    for course in courses:
+        day, start, end = spilt_time(course.time)
+        for i in range( int(start), int(end)+1 ):
+            # print(i)
+            table[day][i].append(course)
+    #     print(int(time[0]),int(time[1]))
+    # print(table)
+    return table
+
+
+def get_course_table(user):
+    credit = [0, 0, 0]
+    for course in user.courses:
+        credit[0] += course.credits
+        if user.dept_name is not None and course.department == user.dept_name:
+            credit[1] += course.credits
+        elif course.dept_code == 'GU':
+            credit[2] += course.credits
+    return credit
 
 
 def get_all_courses_code():
@@ -22,47 +72,64 @@ def get_all_courses_code():
 @login_required
 def table(user):
     # get all courses data
-    print_user(user)
+    credit = get_credits(user)
     courses, dept_name_code = get_all_courses_code()
     user_course = user.courses
+    # parse_course_time(user_course)
 
-    return render_template('table.html', course =courses, db_dept=dept_name_code, user_course = user_course)
+    table = parse_course_time(user_course)
+    user_dept = user.dept_name
+
+    return render_template('table.html', db_dept=dept_name_code, user_course=user_course, credit=credit, table=table, user_dept=user_dept, change_color = None )
+
+
 # return redirect(url_for("login"))
 
 
 @table_bp.route('/add', methods=['POST'])
 @login_required
 def add_course(user):
+    user_course = user.courses
     courses, dept_name_code = get_all_courses_code()
     if request.method == 'POST':
         course_to_be_add = Course.query.filter_by(cid=request.form.get('cid')).first()
         if course_to_be_add not in user.courses:
             user.courses.append(course_to_be_add)
             db.session.commit()
-        print(course_to_be_add.course_name)
-        return render_template('table.html', values=courses, db_dept=dept_name_code)
+            print(course_to_be_add.course_name)
+        credit = get_credits(user)
+        table = parse_course_time(user_course)
+        user_dept = user.dept_name
+        return render_template('table.html', db_dept=dept_name_code, user_course=user_course, credit=credit, table=table, user_dept=user_dept, change_color = None)
 
 
 @table_bp.route('/remove', methods=['POST'])
 @login_required
 def remove_course(user):
-    courses = Course.query.all()
-    dept_name_code = {}
-    for course in courses:
-        dept_name_code[course.dept_code] = course.department
+    user_course = user.courses
+    courses, dept_name_code = get_all_courses_code()
     if request.method == 'POST':
+        print(request.form.get('cid'))
         course_to_be_remove = Course.query.filter_by(cid=request.form.get('cid')).first()
-        user.courses.remove(course_to_be_remove)
-        db.session.commit()
-        print(course_to_be_remove.course_name)
-        return render_template('table.html', values=courses, db_dept=dept_name_code )
+        if course_to_be_remove in user.courses:
+            user.courses.remove(course_to_be_remove)
+            db.session.commit()
+            print(course_to_be_remove.course_name)
+        credit = get_credits(user)
+        table = parse_course_time(user_course)
+        user_dept = user.dept_name
+        return render_template('table.html', db_dept=dept_name_code, user_course=user_course, credit=credit, table=table, user_dept=user_dept , change_color = None)
 
 
 @table_bp.route('/search', methods=['POST'])
-def search_course():
+@login_required
+def search_course(user):
     # 拿 department 的代碼和 department
+    credit = get_credits(user)
     courses, dept_name_code = get_all_courses_code()
-
+    user_course = user.courses
+    table = parse_course_time(user_course)
+    user_dept = user.dept_name
     if request.method == 'POST':
         # 輸入搜尋
 
@@ -85,4 +152,33 @@ def search_course():
         elif dept_code and condition == '':
             course_searched.extend(Course.query.filter_by(dept_code=dept_code).all())
         course_searched = list(course_searched)
-        return render_template('table.html', course_searched=course_searched, db_dept=dept_name_code, condi=condition, dept_selected=dept_name_code.get(dept_code))
+        return render_template('table.html', course_searched=course_searched, db_dept=dept_name_code, condi=condition,
+                               dept_selected=dept_name_code.get(dept_code), user_course=user_course, credit=credit, table=table, user_dept=user_dept, change_color = None)
+
+
+@table_bp.route('/get_table', methods=['GET'])
+@login_required
+def get_table_course(user):
+    credit = get_credits(user)
+    courses, dept_name_code = get_all_courses_code()
+    user_course = user.courses
+    table = parse_course_time(user_course)
+    user_dept = user.dept_name
+    if request.method == 'GET':
+    # 選課志願
+        # print(request.args.get('day'))
+        time = request.args.get('day')
+        print(time)
+        t = time.split('-')
+        print(t[0], t[1])
+        user_course = table[int(t[0])-1][int(t[1])]
+        print(user_course)
+
+        tmp = [int(t[0]), int(t[1])]
+        color_block = []
+        color_block.append(tmp)
+        print( color_block )
+    # 搜尋 --> 晚點做
+
+    return render_template('table.html', db_dept=dept_name_code, user_course=user_course, credit=credit, table=table, user_dept=user_dept, change_color = color_block )
+
